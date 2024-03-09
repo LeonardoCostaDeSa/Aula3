608d1b2743260d421be1509200e03b854736faa5
Leonardo de Sa <leonardocostasa95@gmail.com>
2024-03-09 12:13:43 -0300

    Documentation done

--- multiplication_table.js
+++ multiplication_table.js
@@ -1,9 +1,12 @@
+//We start importing the PromptSync library to create an typeable input to the user:
 import PromptSync from "prompt-sync";
+//Then, we've declared the system variables:
 const prompt = PromptSync()
 const number_requested = Number(prompt("What number do you want to see the multiplication table: "))
 let result = 0
-
+//Repeat loop to print in the screen the multiplication table:
 for (let i=0;i<=10;i++){
     result = number_requested*i
     console.log(`${number_requested}X${i}=${result}`)    
 } 
+//The end
